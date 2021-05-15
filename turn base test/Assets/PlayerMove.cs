d1e3bca6e79e1c686a556c4f8a4cefa3c0832c1b
TyriqPatt <38378678+TyriqPatt@users.noreply.github.com>
2021-05-15 16:35:59 -0400

    Upload project

---
+++ turn base test/Assets/PlayerMove.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMove : TacticsMove
+{
+    public bool UnitIsSelected;
+    public GameObject SelectedIndicator;
+    public GameObject AttackRange;
+    public GameObject Directions;
+    public TurnManager TM;
+    public Animator anim;
+    public Transform f, r, b, l;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (UnitIsSelected)
+        {
+            if (TM.Turns == TurnManager.TurnState.SelectUnit)
+            {
+                FindSelectableTiles();
+                SelectedIndicator.SetActive(true);
+                TM.Turns = TurnManager.TurnState.Move;
+            }
+
+            if (!moving)
+            {
+                //FindSelectableTiles();
+                //TM.CheckMouse();
+            }
+            else
+            {
+                Move();
+                anim.SetBool("isWalking", true);
+                
+            }
+            if (Donemoving)
+            {
+                TM.Turns = TurnManager.TurnState.Attack;
+                AttackRange.SetActive(true);
+                AttackRange.transform.position = this.transform.position;
+                Donemoving = false;
+                anim.SetBool("isWalking", false);
+            }
+        }
+    }
+
+    public void Endturn()
+    {
+        SelectedIndicator.SetActive(false);
+        UnitIsSelected = false;
+        Directions.SetActive(false);
+        AttackRange.transform.position = Vector3.zero;
+        Directions.transform.position = Vector3.zero;
+    }
+
+    public void ChangeDirection(int Dir)
+    {
+        if(Dir == 0)
+        {
+            transform.forward = f.transform.position - transform.position;
+            transform.forward.Normalize();
+        }
+        else if (Dir == 1)
+        {
+            transform.forward = r.transform.position - transform.position;
+            transform.forward.Normalize();
+        }
+        else if (Dir == 2)
+        {
+            transform.forward = b.transform.position - transform.position;
+            transform.forward.Normalize();
+        }
+        else if (Dir == 3)
+        {
+            transform.forward = l.transform.position - transform.position;
+            transform.forward.Normalize();
+        }
+    }
+
+    public void EndAttack()
+    {
+        TM.Turns = TurnManager.TurnState.DirChange;
+        Directions.SetActive(true);
+        Directions.transform.position = this.transform.position;
+    }
+}
---
+++ turn base test/Assets/TurnManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public enum TurnState {SelectUnit, Move, Attack, DirChange, NextTurn }
+
+    public TurnState Turns;
+
+    public PlayerMove pm;
+
+    public int PlayerNum = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateFunction();
+    }
+
+    void StateFunction()
+    {
+        if(Turns == TurnState.SelectUnit)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                if(PlayerNum == 1)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.collider.tag == "Player")
+                        {
+                            pm = hit.transform.GetComponent<PlayerMove>();
+                            pm.UnitIsSelected = true;
+                        }
+                    }
+                }
+
+                if (PlayerNum == 2)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.collider.tag == "Player2")
+                        {
+                            pm = hit.transform.GetComponent<PlayerMove>();
+                            pm.UnitIsSelected = true;
+                        }
+                    }
+                }
+            }
+
+        }
+        else if (Turns == TurnState.Move)
+        {
+            ClickToMove();
+        }
+        else if (Turns == TurnState.Attack)
+        {
+            ClickToAttack();
+        }
+        else if (Turns == TurnState.DirChange)
+        {
+            ClickToChangeDir();
+        }
+        else if (Turns == TurnState.NextTurn)
+        {
+            NextTurn();
+            Turns = TurnState.SelectUnit;
+            pm.Endturn();
+            pm = null;
+        }
+    }
+
+    public void NextTurn()
+    {
+        if(PlayerNum == 1)
+        {
+            PlayerNum = 2;
+        }
+        else if (PlayerNum == 2)
+        {
+            PlayerNum = 1;
+        }
+    }
+
+    public void ClickToMove()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Tile")
+                {
+                    Tile t = hit.collider.GetComponent<Tile>();
+                    if (t.selectable)
+                    {
+                        pm.MoveToTile(t);
+                        Turns = TurnState.Move;
+                    }
+                }
+            }
+        }
+    }
+
+    public void ClickToAttack()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Attackable")
+                {
+
+                    if(hit.collider.name == "FrontDir")
+                    {
+                        pm.ChangeDirection(0);
+                    }
+
+                    else if (hit.collider.name == "RightDir")
+                    {
+                        pm.ChangeDirection(1);
+                    }
+
+                    else if (hit.collider.name == "BackDir")
+                    {
+                        pm.ChangeDirection(2);
+                    }
+
+                    else if (hit.collider.name == "LeftDir")
+                    {
+                        pm.ChangeDirection(3);
+                    }
+                    pm.anim.SetTrigger("Hit");
+                    pm.AttackRange.SetActive(false);
+
+                    //Turns = TurnState.NextTurn;
+                }
+            }
+        }
+    }
+
+    public void ClickToChangeDir()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Attackable")
+                {
+
+                    if (hit.collider.name == "FrontDir")
+                    {
+                        pm.ChangeDirection(0);
+                    }
+
+                    else if (hit.collider.name == "RightDir")
+                    {
+                        pm.ChangeDirection(1);
+                    }
+
+                    else if (hit.collider.name == "BackDir")
+                    {
+                        pm.ChangeDirection(2);
+                    }
+
+                    else if (hit.collider.name == "LeftDir")
+                    {
+                        pm.ChangeDirection(3);
+                    }
+                   Turns = TurnState.NextTurn;
+                }
+            }
+        }
+    }
+
+}
